c93194ace8426abdc5dadfd5f3eebfbfbb0f1a95
Matthew Bastow <matthewbastow93@gmail.com>
2024-10-28 22:24:55 +0000

    ✨ can add new name to selection.

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -14,7 +14,6 @@ pub fn Home() -> impl IntoView {
     let emoji_list = get_emojis();
 
     view! {
-        // Makes the page the size of the current window and centres all items.
         <div class="min-h-screen w-full flex items-center justify-center p-4">
             <CardGrid names emoji_list/>
         </div>
@@ -24,21 +23,36 @@ pub fn Home() -> impl IntoView {
 #[component]
 pub fn CardGrid(names: RwSignal<Vec<RwSignal<String>>>, emoji_list: Vec<String>) -> impl IntoView {
     view! {
-        // Put the card in the centre of the window, place gaps between each
-        // one and create new lines of cards if the screen gets too small.
         <Flex gap=FlexGap::Small justify=FlexJustify::Center class="flex-wrap">
             // Create one card for every name.
             <For each=move || names.get() key=|name| name.clone() let:name>
                 <NameCard name emoji_list=emoji_list.clone()/>
             </For>
+            // Button for adding a new card.
+            <Button
+                appearance=ButtonAppearance::Transparent
+                icon=icondata::ChPlus
+                on_click=move |_| names.update(|names| names.push(RwSignal::new(String::new())))
+            >
+                "New"
+            </Button>
         </Flex>
     }
 }
 
 #[component]
 pub fn NameCard(name: RwSignal<String>, emoji_list: Vec<String>) -> impl IntoView {
+    let input_ref = ComponentRef::<InputRef>::new();
+
+    // Create an effect that runs after the component is mounted
+    Effect::new(move |_| {
+        // Focus the input if it exists
+        if let Some(input) = input_ref.get_untracked() {
+            input.focus()
+        }
+    });
+
     view! {
-        // Stop the card from shrinking
         <div class="grow-0 shrink-0 basis-20 sm:basis-28 md:basis-40">
             <Card class="w-full h-full p-0 border-slate-200 border-2 rounded">
                 // A picture to help identify the name.
@@ -49,10 +63,7 @@ pub fn NameCard(name: RwSignal<String>, emoji_list: Vec<String>) -> impl IntoVie
                 </CardPreview>
                 // The editable name.
                 <div class="p-2.5">
-                    <Input
-                        value={name}
-                        class="border-none text-center w-28 sm:w-40 md:w-52"
-                    />
+                    <Input value=name comp_ref=input_ref class="border-none text-center w-28 sm:w-40 md:w-52"/>
                 </div>
             </Card>
         </div>
