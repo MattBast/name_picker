5ef2772f84766bbfad46aec3088e02b5f42f52a6
Matthew Bastow <matthewbastow93@gmail.com>
2024-11-20 20:15:35 +0000

    💄 added content to welcome page.

--- src/components/bottom_nav.rs
+++ src/components/bottom_nav.rs
@@ -5,10 +5,45 @@ use leptos::prelude::*;
 pub fn BottomNav(people: RwSignal<Vec<Person>>, children: Children) -> impl IntoView {
     view! {
         <div
-            class="w-full py-4 bg-yellow-300"
+            class="w-full py-4 bg-yellow-300 flex flex-col justify-center"
             class: h-full=move || people.get().is_empty()
         >
-            <div class="flex justify-center items-center gap-2 w-full h-full">
+            // This contains text to explain what the app does to the user.
+            <div
+                class="w-full p-4 text-center"
+                class: hidden=move || !people.get().is_empty()
+            >
+                <div
+                class="flex justify-center items-center text-4xl gap-4"
+                >
+                    <div>
+                        {emojis::get("😀").unwrap().as_str()}
+                    </div>
+                    <div class="rotate-45 -translate-y-1 text-6xl">
+                        {emojis::get("🥳").unwrap().as_str()}
+                    </div>
+                    <div>
+                        {emojis::get("😎").unwrap().as_str()}
+                    </div>
+                    <div>
+                        {emojis::get("😁").unwrap().as_str()}
+                    </div>
+                </div>
+                <h1
+                    class="font-normal text-6xl tracking-wide font-sans subpixel-antialiased text-slate-800"
+                >
+                    "Name Picker"
+                </h1>
+                <h2
+                    class="font-normal text-3xl tracking-normal leading-normal font-sans antialiased text-slate-600"
+                >
+                    "Write a list of names and randomly select one."
+                </h2>
+            </div>
+
+            // This contains the control panel buttons that the user will use to
+            // interact with the name picker app.
+            <div class="flex justify-center items-center gap-2 w-full">
                 {children()}
             </div>
         </div>
