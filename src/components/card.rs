51badeb9752cffa2e176051131fc30c93df797e9
Matthew Bastow <matthewbastow93@gmail.com>
2024-11-21 22:27:25 +0000

    🐛 card no longer squeezes content onto two rows and instead keeps everything on one.

--- src/components/card.rs
+++ src/components/card.rs
@@ -21,16 +21,16 @@ where
 {
     view! {
         <div
-            class="flex-initial flex flex-col md:flex-row w-full my-2 bg-white shadow-sm border border-slate-200 rounded-lg w-96 transition-all overflow-hidden"
+            class="flex-initial flex md:flex-row w-full my-2 bg-white shadow-sm border border-slate-200 rounded-lg w-96 transition-all overflow-hidden"
             class: opacity-50=move || not_picked.get()
             class: scale-95=move || not_picked.get()
             class: scale-105=move || picked.get()
         >
             // The random emoji that provides a visual identity for the person.
-            <div class="px-4 py-2.5 text-3xl flex items-center justify-center bg-blue-400 mr-2">
+            <div class="flex-none px-4 py-2.5 text-3xl flex items-center justify-center bg-blue-400 mr-2">
                 {random_emoji(emoji_list)}
             </div>
-            <div class="flex items-center w-full justify-between">
+            <div class="flex-auto flex items-center justify-between">
                 // The editable persons name.
                 <h4 class="mb-2 text-slate-800 text-xl font-semibold w-full">
                     <NameInput name on_keyboard_event on_blur_event node_ref/>
