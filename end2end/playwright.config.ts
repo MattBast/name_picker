9b00ac9f9591aa3d18ba8a13a395305171d29766
Matthew Bastow <matthewbastow93@gmail.com>
2025-01-19 14:19:19 +0000

    🧪 added playwright report upload to not cancelled CI runs.

--- end2end/playwright.config.ts
+++ end2end/playwright.config.ts
@@ -31,6 +31,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  /* Stop after 5 failures in CI */
+  maxFailures: process.env.CI ? 5 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
